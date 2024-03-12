98168fa19e2dd1ac4fda93f0b519d1c2ea3720ed
Maxwell Chalmers <mchalme2@unca.edu>
2024-03-12 17:17:25 -0400

    init

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+import requests
+import click 
+
+
+import click
+
+
+@click.group()
+def gif():
+    print("hello from giphy cli!")
+
+
+@gif.command()
+def trending():
+    print("trending subcommand called!")
+
+
+@gif.command()
+def search():
+    print("search subcommand called!")
+
+
+def buildSearchCall(KEY,
+                    query = "cats", 
+                    limit = 5,
+					offset = 0,
+                    rating = "g",
+                    lang = 'en', 
+                    random_id = "bob", 
+                    bundle = "messaging_non_clips"):
+    print("foo")
+      
+
+def buildTrendingCall(KEY, 
+                      limit = 5, 
+                      offset = 0, 
+                      rating = "g", 
+                      bundle = "messaging_non_clips" ):
+    print("woo")
+      
+
+if __name__ == "__main__":
+	API_KEY = os.environ["GIF_KEY"] 
+	apiCall = "https://api.giphy.com/v1/gifs/trending?api_key=" + API_KEY + "&limit=1&offset=0&rating=g" 
+
+	resp = requests.get(apiCall)
+
+	gify = resp.json()
+
+	
+	print(gify.keys())
+	
+	data = gify.get("data").pop()
+	for k in data.keys():
+		print (k + " = " + str(data.get(k)) + "\n")
+
+
+	gif()
